4fb9e9c1f035e49647b1767cfd8547f867a08d2d
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-03-14 12:00:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// ZGADNIJ PWA Service Worker (BUILD 3004)
+// ZGADNIJ PWA Service Worker (BUILD 3005)
 const CACHE_NAME = 'typer-cache-8037';
 
 // Core assets to pre-cache (keep minimal to avoid stale UI)
